059e5d790dcf81af63ac891df30ed4a0e7fa9954
우영창 <dudckd@uyeongchang-ui-MacBookPro.local>
2021-02-11 21:56:51 +0900

    first commit

--- server/index.js
+++ server/index.js
@@ -6,10 +6,10 @@ const cors = require('cors')
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const config = require("./config/key");
+// const config = require("./config/key");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
+const connect = mongoose.connect('mongodb+srv://dudckd:013111@youtb.mdyrj.mongodb.net/<docker>?retryWrites=true&w=majority',
   {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex: true, useFindAndModify: false
